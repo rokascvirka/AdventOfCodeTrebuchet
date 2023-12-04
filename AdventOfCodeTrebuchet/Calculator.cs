e581cb947bc54bb937ed484e48284a0fd83edb9f
RokasCvirka <rokas42@gmail.com>
2023-12-04 13:23:58 +0200

    Add Calculator

---
+++ AdventOfCodeTrebuchet/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCodeTrebuchet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeTrebuchet
+{
+    public class Calculator
+    {
+        public static void CalculateSum(List<Coordinates> coordinates)
+        {
+            var sum = 0;    
+            foreach (var cord in coordinates)
+            {
+                Console.WriteLine($"Coordinates: {cord.Coords}");
+                sum += cord.Coords;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
--- AdventOfCodeTrebuchet/Program.cs
+++ AdventOfCodeTrebuchet/Program.cs
@@ -7,14 +7,8 @@ namespace AdventOfCodeTrebuchet
         static void Main(string[] args)
         {
             var FILE_PATH = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "", "Coordinates.txt");
-            var sum = 0;
             var coordinates = FileManager.ReadFile(FILE_PATH);
-            foreach (var cord in coordinates)
-            {
-                Console.WriteLine($"Coordinates: {cord.Coords}");
-                sum += cord.Coords;
-            }
-            Console.WriteLine(sum);
+            Calculator.CalculateSum(coordinates);
         }
     }
 }
